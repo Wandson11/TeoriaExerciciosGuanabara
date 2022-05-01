2062d2d1e755a66bceb0ab9524ada97b2c88e7dc
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:30:59 -0400

    Update desafio07.py

--- Cap07operadoresArit/desafio07.py
+++ Cap07operadoresArit/desafio07.py
@@ -1,3 +1,5 @@
+# exercício para calcular a média de duas notas.
+
 nota1 = float(input('Digite a nota da primeira prova: '))
 nota2 = float(input('Digite a nota d segunda prova: '))
 print(f'A média entre {nota1} e {nota2} é igual a {round((nota1+nota2)/2, 1)}')
