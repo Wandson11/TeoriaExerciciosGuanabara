0c7de59f8ef8070f642a285af8886b7149a51374
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 01:24:49 -0400

    Update aula08b.py

--- Cap8Importacao/aula08b.py
+++ Cap8Importacao/aula08b.py
@@ -1,2 +1,6 @@
 import emoji
 print(emoji.emojize('Olá, Mundo :earth_americas:', use_aliases= True))
+
+# pelo o que puder ver no stack overflow, o use_aliases foi aplicado com o objetivo de ampliar a quantidade de emoticons, além dos oficiais disponibilizados:
+# http://www.unicode.org/emoji/charts/full-emoji-list.html
+# lista de emoticos que adentram pelo referido comando https://www.webfx.com/tools/emoji-cheat-sheet/
