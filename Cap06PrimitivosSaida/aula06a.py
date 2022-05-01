ac6bf882a86a33c16c237ea41ae8cfe92c3cae88
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 13:44:23 -0400

    Add files via upload

---
+++ Cap06PrimitivosSaida/aula06a.py
@@ -0,0 +1,11 @@
+peixe = str(('Desafio 01'))
+print(f'{peixe:=^20}')
+#n1 = int(input('Digite um valor: '))
+#n2 = int(input('Digite outro valor: '))
+#s = n1 + n2
+# print('A soma entre {}'.format(n1), 'e {}'.format(n2), 'vale {}'.format(s))
+# print('A soma do valor de {} e de {} vale {}'.format(n1, n2, s))
+#print(f'A soma do valor de {n1} e {n2} vale {s}')
+
+
+
---
+++ Cap06PrimitivosSaida/desafio04.py
@@ -0,0 +1,3 @@
+n = input('Digite algo aqui: ')
+print(type(n))
+print(n.isnumeric())
