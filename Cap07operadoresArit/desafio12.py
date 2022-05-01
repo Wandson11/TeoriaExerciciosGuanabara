785fa41211fe43d15cd4a678e82b837759c6264c
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:35:32 -0400

    Update desafio12.py

--- Cap07operadoresArit/desafio12.py
+++ Cap07operadoresArit/desafio12.py
@@ -1,3 +1,5 @@
+# apurar o desconto de um produto
+
 produto = float(input('Qual é o preço do produto? R$ '))
 desconto = produto * 0.05
 print(f'O produto que custava {produto}, na promoção com desconto de 5% vai custar R$ {round(produto - desconto, 5)}')
