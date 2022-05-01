5a2f25b6606ba8f867b9d1d5813a13039d752c4d
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:36:30 -0400

    Update desafio13.py

--- Cap07operadoresArit/desafio13.py
+++ Cap07operadoresArit/desafio13.py
@@ -1,3 +1,5 @@
+# apurar o reauste salarial de acordo com o input e os 15% previsto do exemplo.
+
 vencimentos = float(input('Qual é o salario do funcionário? '))
 reajuste = vencimentos * 0.15
 print(f'Um funcionário que ganhava R$ {vencimentos}, com 15% de aumento, passa a receber R$ {round(vencimentos + reajuste, 2)}')
