03baccfe20a1ceeacc02085814205ab24f9ac888
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 01:16:12 -0400

    Delete aula08a.py

--- aula08a.py
+++
@@ -1,6 +0,0 @@
-# ensinamento sobre as opçoes e a importaçao do modulo
-
-from math import sqrt, floor
-num = int(input('Digite um número: '))
-raiz = sqrt(num)
-print(f'A raiz quadrada de {num} é {floor(raiz)}')
