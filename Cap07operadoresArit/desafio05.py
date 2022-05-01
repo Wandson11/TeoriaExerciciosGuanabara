1dabebbd0375bed6a520aec31acc8abc0b30a6fb
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:16:59 -0400

    Update desafio05.py

--- Cap07operadoresArit/desafio05.py
+++ Cap07operadoresArit/desafio05.py
@@ -1,4 +1,5 @@
-# calcular o antecessor e sucessor de um número digitado no input, usei a variavel n1 com a adiçao e a subtraçao dentro das chaves.No exemplo da aula, foram criadas duas variaveis, uma para cada operaçao
+# calcular o antecessor e sucessor de um número digitado no input, 
+# usei a variavel n1 com a adiçao e a subtraçao dentro das chaves.No exemplo da aula, foram criadas duas variaveis, uma para cada operaçao.
 
 n1 = int(input('Escreve um número: '))
 print(f'Analisando o valor {n1} é possivel afirmar que o seu antecessor é {n1-1} e o seu sucessor {n1+1}')
