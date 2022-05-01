dbb4167c1653fd8c53bdbfe3ace08e97cc8de5b3
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:32:00 -0400

    Update desafio08.py

--- Cap07operadoresArit/desafio08.py
+++ Cap07operadoresArit/desafio08.py
@@ -1,2 +1,4 @@
+# exercício para calcular uma distancia em metro para cm, mm, dm, dam.
+
 t = float(input('Uma distancia em metro: '))
 print(f'A medida de {t} corresponde\n{t*100}cm\n{t*1000}mm\n{t*10}dm\n{t/10}dam')
