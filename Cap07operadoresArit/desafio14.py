8fb58d56261b67f0d48861913546e42d3379bb1a
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:37:44 -0400

    Update desafio14.py

--- Cap07operadoresArit/desafio14.py
+++ Cap07operadoresArit/desafio14.py
@@ -1,3 +1,5 @@
+# exercício para apurar a conversão de grau celsius para fahrenheit
+
 tempo = float(input('Informe a temperatura em ºC '))
 f = tempo * 1.8 + 32
 print(f'A temperatura de {tempo}º corresponde a {f}ºF!')
