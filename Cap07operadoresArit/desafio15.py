f5a99c1e4a84f9d5f20a1d381678a8cc257a707a
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-01 11:38:34 -0400

    Update desafio15.py

--- Cap07operadoresArit/desafio15.py
+++ Cap07operadoresArit/desafio15.py
@@ -1,3 +1,5 @@
+# exercício para apurar o valor a ser pago de um aluguel de carro, com valor diário a ser pago e por km percorrido.
+
 dia = int(input('Quantos dias alugados? '))
 km = float(input('Quantos km rodados? '))
 valordia = dia * 60
