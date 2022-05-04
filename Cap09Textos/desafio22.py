c4aeadae94f775fd7f1ef66a8b6ea1fac789eaaa
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-03 23:24:50 -0400

    Add files via upload

---
+++ Cap09Textos/desafio22.py
@@ -0,0 +1,20 @@
+# Crie um programa que leia o nome completo de uma pessoa e mostre
+
+#minha resolução
+'''nome = str(input('Digite o seu nome: '))
+print(f'Todas as letras maiúsculas {nome.upper()}')
+print(f'Todas as letras minúsculas {nome.lower()}')
+cortazero = nome.split()
+sumindocomzero = nome.replace(" ", "")
+contagemsemzero = len(sumindocomzero)
+print(contagemsemzero)
+print(f'Quantas letras tem o primeiro nome? {len(cortazero[0])}')'''
+
+#resolução da aula
+nome = str(input('Digite o seu nome completo: ')).strip()
+print(f'Seu nome é maiúscula é {nome.upper()}')
+print(f'Seu nome é minúsculo é {nome.lower()}')
+print(f'Seu nome tem ao todo {len(nome) - nome.count(" ")} letras')
+# print(f'Seu primeiro nome tem {nome.find(" ")} letras') o índice começa pelo zero
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e tem {len(separa[0])} letras')
