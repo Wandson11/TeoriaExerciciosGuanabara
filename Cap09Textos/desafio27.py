27ca289a5e427d4628c30336ac4e3d62340130a1
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-04 23:31:49 -0400

    Update desafio27.py

--- Cap09Textos/desafio27.py
+++ Cap09Textos/desafio27.py
@@ -11,4 +11,5 @@ n = str(input('Digito seu nome completo: ')).strip()
 nome = n.split()
 print(f'Seu primeiro nome é {nome[0]}')
 print(f'Seu último nome é {nome[len(nome)-1]}')
-
+#com as alteraçoes de 2019, essa linha de cima pode ser escrita assim: print(f'Seu último nome é {nome[-1]}')
+#no comentários do vídeo, é indicado que o menos -1, inverte a ordem: Peixe(-3) Boi (-2) Azul (-1)
