a2f15e518d85671ab003e9ab14b46564aec3b622
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-04 22:46:05 -0400

    Update desafio25.py

--- Cap09Textos/desafio25.py
+++ Cap09Textos/desafio25.py
@@ -1,4 +1,16 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem 'Silva' no nome
 
-nome = str(input('Digite seu nome completo: '))
-print('Silva' in nome)
+#essa foi a primeira versão
+'''nome = str(input('Digite seu nome completo: '))
+print('Silva' in nome)'''
+
+#ajustei posteirormente para isso, no caso inseri title para padronizar a leitra dos nomes, conforme a aula.
+#na aula usuou exemplo como lower e upper
+#mas novamente não está perfeito, pois reconhece "silvana" como True
+'''nome = str(input('Digite seu nome completo: ')).strip()
+print('Silva' in nome.title())'''
+
+#agora correto
+nome = str(input('Digite o seu nome completo: ')).strip()
+nomealterado = nome.upper().split()
+print('SILVA' in nomealterado[:])
