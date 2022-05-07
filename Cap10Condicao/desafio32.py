54d1403cf139c052940b14208a1d8e15e2adc703
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-06 23:25:19 -0400

    Add files via upload

---
+++ Cap10Condicao/desafio32.py
@@ -0,0 +1,7 @@
+# escreve um ano e mostre se ele é bissexto
+
+bissexto = int(input('É um ano bissexto? '))
+if (bissexto % 4) == 0:
+    print('É um anexo bissexto')
+else:
+    print('Nao é um ano bissexto')
