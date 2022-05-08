52885c47833de5a4ea325c8b103da6f8205e5385
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-07 21:59:45 -0400

    Update desafio29.py

--- Cap10Condicao/desafio29.py
+++ Cap10Condicao/desafio29.py
@@ -3,8 +3,9 @@
 #A multa vai custar R$ 7,00 por cada km acima do limite
 
 zum = float(input('Velocidade máxima atingida: '))
-multa = (zum * 7.0)
+multa = ((zum - 80) * 7.0)
 if zum >= 80:
     print(f'Você foi multado por ter atingido velocidade acima do permitido, multa de R$ {round(multa,2)}')
 else:
     print('')
+  # ficou funcional igual ao da aula mas a estrutura feita foi de forma diferente (condiçao simples)
