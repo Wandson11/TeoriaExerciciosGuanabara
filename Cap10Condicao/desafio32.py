1ebe74ec30ba3c6f47b67e0fce55c376382020f4
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-07 22:58:15 -0400

    Update desafio32.py

--- Cap10Condicao/desafio32.py
+++ Cap10Condicao/desafio32.py
@@ -1,7 +1,17 @@
 # escreve um ano e mostre se ele é bissexto
 
-bissexto = int(input('É um ano bissexto? '))
+'''bissexto = int(input('É um ano bissexto? '))
 if (bissexto % 4) == 0:
-    print('É um anexo bissexto')
+    print('É um ano bissexto')
 else:
-    print('Nao é um ano bissexto')
+    print('Nao é um ano bissexto')'''
+
+#exercicio resolvido, nota-se a utilizaçao de 'and' e 'or'
+from datetime import date
+bissexto = int(input('É um ano bissexto? Coloque 0 para analisar o ano atual: '))
+if bissexto == 0: #isso daqui é para pegar a data referência no dia que tiver fazendo.
+    bissexto = date.today().year
+if bissexto % 4 == 0 and bissexto % 100 != 0 or bissexto % 400 == 0:
+    print(f'O ano de {bissexto} é bissexto')
+else:
+    print(f'O ano {bissexto}, NÃO é bissexto')
