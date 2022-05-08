82ba0fea3ebe77425d3c00d9f095e88eb40460c8
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-07 22:36:53 -0400

    Update desafio31.py

--- Cap10Condicao/desafio31.py
+++ Cap10Condicao/desafio31.py
@@ -1,7 +1,13 @@
 # distancia de uma viagem em km, preço da passagem, R$ 0,50/km para te 200 km e 0,45 para mais longas
 
-distancia = float(input('Qual é a distancia? '))
+'''distancia = float(input('Qual é a distancia? '))
 if distancia <= 200:
     print(f'O preço da passagem será R$ {distancia * 0.50}')
 else:
-    print(f'O preço da passagem será de R$ {distancia * 0.45}')
+    print(f'O preço da passagem será de R$ {distancia * 0.45}')'''
+
+#modelo feito em aula, na forma simplificada.
+distancia = float(input('Qual é a distancia da viagem?'))
+print(f'Voce está prestes a começar uma viagem de {distancia}')
+preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print(f'E o preço de sua passagem será de {preco}')
