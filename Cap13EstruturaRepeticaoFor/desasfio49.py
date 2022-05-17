f47929d0f7dfadb4a103761ae9cacbb9b498e7ea
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-16 23:14:05 -0400

    Add files via upload

---
+++ Cap13EstruturaRepeticaoFor/desasfio49.py
@@ -0,0 +1,11 @@
+#fazer tabuada
+
+'''taboa = int(input('Qual é a tabuada que você quer ver? '))
+print(f'Taboada de {taboa}')
+for c in range(1, 11):
+    conta = c * taboa
+    print(f'{taboa} x {c} = {conta}')'''
+
+tabua = int(input('Tabuada: '))
+for c in range(1, 11):
+    print(f' a tabuada de {tabua} x {c}, fica {tabua * c}')
