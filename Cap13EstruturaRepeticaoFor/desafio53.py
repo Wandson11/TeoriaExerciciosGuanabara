355dd425387bcef045fea0b254d1bf1c14ebb051
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-21 15:03:21 -0400

    Add files via upload

---
+++ Cap13EstruturaRepeticaoFor/desafio53.py
@@ -0,0 +1,28 @@
+#detector de palíndromo
+
+'''n1 = str(input('Digite uma frase: ')).upper()
+conjunto = ['APOS A SOPA', 'A SACADA DA CASA', 'A TORRE DA DERROTA', 'O LOBO AMA O BOLO', 'ANOTARAM A DATA DA MARATONA']
+inverso = n1[::-1]
+print(f' Voce digitou {n1.replace(" ","")}, que ao contrário, significa {inverso.replace(" ", "")}')
+for n1 in conjunto:
+    print(f'a {"APOS A SOPA" in n1}')'''
+
+#nao consegui resolver esse desafio.
+
+#resoluçao do guanabara
+frase = str(input('Escreva uma frase: ')).upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+#dá para fazer sem o for, no caso, uso o inverso = junto[::-1], essa ideia cheguei a usar no meu modelo.
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('temos um palíndromo')
+else:
+    print('Nao é um palíndromo')
+
+
+
+
