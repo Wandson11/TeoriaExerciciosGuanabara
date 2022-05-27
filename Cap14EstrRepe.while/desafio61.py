815761ae6ed1e60be067585935aab169d3342488
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-05-26 21:46:44 -0400

    Update desafio61.py

--- Cap14EstrRepe.while/desafio61.py
+++ Cap14EstrRepe.while/desafio61.py
@@ -14,4 +14,24 @@ for c in range(n1, enesima + n2, n2):
 n1 = int(input('Digite o termo inicial: '))
 n2 = int(input('Digite a razão: '))
 
-while 
+#resoluçao vista nos comentários.
+'''n1 = int(input('Digite o termo inicial: '))
+n2 = int(input('Digite a razão: '))
+enesimo = n1 + (10-1) * n2
+c = 10 # nao entendi isso aqui
+while c > 0:
+    print(f'{n1}')
+    n1 = n1 + n2
+    c = c -1
+print('fechando')'''
+
+#nao precisa da enésima
+#resoluçao do guanabara.
+primeiro = int(input('Digite o primeiro termo: '))
+razao = int(input('Digite a razão: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print(f'{termo}')
+    termo += razao
+    cont += 1
