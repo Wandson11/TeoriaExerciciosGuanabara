2dbcc36256fe39977ebb1c4e6e6c4b558b5ffb3d
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-06-01 21:43:37 -0400

    Update desafio66.py

--- Cap15BreakemWhile/desafio66.py
+++ Cap15BreakemWhile/desafio66.py
@@ -1,6 +1,6 @@
 #digite valores até que seja interrompido e ao final, apresente quantos números foram digitados e o seu somatório
 
-numero = int(input('Digite um número: [999 para a contagem]: '))
+'''numero = int(input('Digite um número: [999 para a contagem]: '))
 somatório = quantidade = 0
 
 while True:
@@ -9,4 +9,14 @@ while True:
     numero = int(input('Digite um número: [999 para a contagem]: '))
     if numero == 999:
         break
-print(f'A soma de {quantidade} valores foi de {somatório}')
+print(f'A soma de {quantidade} valores foi de {somatório}')'''
+
+soma = cont = 0
+while True:
+    num = int(input('Digite um valor: 999 pra parar: '))
+    cont += 1
+    soma += num
+    if num == 999:
+        break
+
+print(f'Foram digitados {cont} que somados perfazem o valor de {soma}')
