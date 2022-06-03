2ba99a6d266cd1b4618389149f578119022cc295
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-06-02 23:18:53 -0400

    Update desafio68.py

--- Cap15BreakemWhile/desafio68.py
+++ Cap15BreakemWhile/desafio68.py
@@ -3,7 +3,7 @@
 #mostrando o total de vitorias consecutivas da pessoa
 
 
-from random import randint
+'''from random import randint
 computador = randint(0, 10)
 soma = 0
 conta = 0
@@ -20,4 +20,34 @@ while True:
     print(f'O computador jogou {computador} e você jogou {valor}, totalizando {conta} = {jogador}')
     soma += 1
     computador = 0
-print(f'Você perdeu, deu {conta}, total de acertos {soma}')
+print(f'Você perdeu, deu {conta}, total de acertos {soma}')'''
+
+#resolução do guanabara
+from random import randint
+v = 0
+
+while True:
+    jogador = int(input('Digite um valor: '))
+    computador = randint(0, 10)
+    total =computador + jogador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou ímpar: [P/I]: ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}', end = ' ')
+    print('Deu par' if total % 2 == 0 else 'Deu impar')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu')
+            v += 1
+        else:
+            print('Voce perdeu')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Voce venceu')
+            v += 1
+        else:
+            print('Você perdeu')
+            break
+    print('Vamos jogar novamente')
+print(f'Game Over, você venceu {v} vez(es)')
