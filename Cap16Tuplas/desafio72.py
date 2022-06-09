4af2f871335ab0e5cb2809efbdfcb9b383131c34
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-06-09 16:37:03 -0400

    Update desafio72.py

--- Cap16Tuplas/desafio72.py
+++ Cap16Tuplas/desafio72.py
@@ -2,7 +2,7 @@
 # o programa deve ler o numero digitado e apresentar o valor por extenso.
 
 
-extenso = 'zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'
+'''extenso = 'zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'
 numero = 0
 
 if numero < 11:
@@ -11,6 +11,36 @@ if numero < 11:
         numero = int(input('Inválido, tente novamente, Digite um número entre 0 e 10: '))
     print(extenso[numero])
 
-print('Fim')
+print('Fim')'''
+
+#acrescentando o continuar
+'''extenso = 'zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'
+numero = 0
+
+if numero < 11:
+    numero = int(input('Digite um número entre 0 e 10: '))
+    while numero >= 11 or numero < 0:
+        numero = int(input('Inválido, tente novamente, Digite um número entre 0 e 10: '))
+    print(extenso[numero])
+    continuar = str(input('Deseja continuar? [S/N]: ')).upper()[0]
+    if continuar == 'S':
+        numero = int(input('Digite um novo número entre 0 e 10: '))
+        while numero >= 11 or numero < 0:
+            numero = int(input('Inválido, tente novamente, Digite um número entre 0 e 10: '))
+    print(extenso[numero])
+print('Fim')'''
+
+#resolução do guanabara
+
+'''cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nova', 'dez',
+        'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito',
+        'dezenove', 'vinte')
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        break
+    print('Tente novamente.', end= ' ')
+print(f'Voce digitou o número {cont[num]}')'''
 
 
