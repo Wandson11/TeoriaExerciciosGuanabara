a070c3f70724f2b03f5f5dc8246993d639bd41fd
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-06-19 20:48:29 -0400

    Update desafio88.py

--- Cap18ListasdentrodeLista/desafio88.py
+++ Cap18ListasdentrodeLista/desafio88.py
@@ -41,3 +41,21 @@ for i, l in enumerate(jogos):
     print(f'Jogo {i + 1}: {l}')
     sleep(0.4)
 print('-=' * 5, f'< BOA SORTE >', '-=' * 5)
+
+#ajsutei o meu modelo, com a resoluçao oficial do Guanabara.
+'''from random import randint
+from time import sleep
+loteria = int(input('Quantos jogos voce quer fazer? '))
+lista = list()
+principal = list()
+for c in range(0, loteria):
+    for n in range(0, 6):
+        jogo = randint(1, 60)
+        if jogo not in lista: #aqui, para nao repetir o número em uma lista.
+            lista.append(jogo)
+    lista.sort()
+    principal.append(lista[:])
+    print(f'Jogo {c+1} {lista}')
+    sleep(0.5)
+    lista.clear()
+print(principal)'''
