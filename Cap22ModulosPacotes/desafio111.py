46c606f32c0dbf35b684eb693cfad029dc5381cd
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-07-03 16:09:35 -0400

    Add files via upload

---
+++ Cap22ModulosPacotes/desafio111.py
@@ -0,0 +1,6 @@
+#continuar com o desafio 110
+#mas um pacote chamado "utilidadesCeV" que tenha 2 módulos internos: moeda e dados
+
+from utilidadesCev import moeda
+n = float(input('Digite o preço: R$ '))
+moeda.resumo(n, 50, 35)
---
+++ Cap22ModulosPacotes/desafio111moeda.py
@@ -0,0 +1,29 @@
+def metade(preco=0, formato=False):
+    resposta = preco/2
+    return resposta if formato is False else moeda(resposta)
+
+def dobro(preco=0, formato=False):
+    resposta = preco*2
+    return resposta if formato is False else moeda(resposta)
+
+def aumentar(preco=0, taxa=0, formato=False):
+    resposta = preco + (preco * taxa/100)
+    return resposta if formato is False else moeda(resposta)
+
+def diminuir(preco=0, taxa=0, formato=False):
+    resposta = preco - (preco*taxa/100)
+    return resposta if formato is False else moeda(resposta)
+
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda} {preco:.2f}'.replace('.', ',')
+
+def resumo(preco=0, taxamaior=0, taxamenor=0):
+    titulo = 'Resumo do Valor'
+    print('=-=' * len(titulo))
+    print(f'{titulo:^40}')#aqui, pode ser usado assim: 'resumo do valor'.center(30), esse 30, foi aleatório
+    print('=-='*len(titulo))
+    print(f'Preco Analisado \t{moeda(preco)}')
+    print(f'Dobro do Preço \t\t{dobro(preco, True)}')
+    print(f'Metade do Preço \t{metade(preco, True)}')
+    print(f'{taxamaior}% de Aumento \t\t{aumentar(preco, taxamaior, True)}')
+    print(f'{taxamenor}% de Redução \t\t{diminuir(preco, taxamenor, True)}')
