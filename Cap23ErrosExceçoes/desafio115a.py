d4a722c55f21ba044c1e41f8c7a6e26e7534a637
Wandson11 <54826954+Wandson11@users.noreply.github.com>
2022-07-09 21:20:40 -0400

    Update desafio115a.py

--- Cap23ErrosExceçoes/desafio115a.py
+++ Cap23ErrosExceçoes/desafio115a.py
@@ -1,7 +1,34 @@
-'''def cadastrar(idade=0, nome=''):
-    idade = idade
-    nome = nome
-    lista = list()
-    if escolha <
-    lista[0].append(idade)
-    lista[1].append(nome)'''
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: por favor, digite um número inteir válido.\033[m')
+            continue
+        except (KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+def linha(tam=42):
+    return '-' * tam
+
+
+def cabecalho(txt):
+    print(linha())
+    print(txt.center(42))
+    print(linha())
+
+
+def menu(lista):
+    cabecalho('MENU PRINCIPAL')
+    c = 1
+    for item in lista:
+        print(f'\033[33m{c}\033[m - \033[34m{item}\033[m')
+        c += 1
+    print(linha())
+    opc = leiaInt('\033[32mSua Opção: \033[m')
+    return opc
+
